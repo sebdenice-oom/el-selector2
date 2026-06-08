507aa507467834c7731d0f434f57822205c329cf
sebdenice-oom <sebdenice@gmail.com>
2026-06-08 22:53:37 +0200

    Update route.js

--- app/api/upgrade-score/route.js
+++ app/api/upgrade-score/route.js
@@ -44,6 +44,7 @@ export async function POST(request) {
       stock: r.stock, precommande: r.precommande || false,
       genre: r.genre, poids: r.poids, schema: r.schema,
       scoreFinal: r.scoreFinal, scoreTech: r.scoreTech,
+      isJoffrey: r.isJoffrey || false,
     }))
 
     return Response.json({ resultats })
